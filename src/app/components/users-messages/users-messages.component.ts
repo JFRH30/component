56862c22530703b57da63da580faa07971bffb64
Alaude30 <kuramamode11@gmail.com>
2018-09-13 19:49:02 +0800

    add user-messages component

---
+++ src/app/components/users-messages/users-messages.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'users-messages-component',
+  templateUrl: './users-messages.component.html',
+  styleUrls: ['./users-messages.component.scss']
+})
+export class UsersMessagesComponent {}
