e2624669bfd238c3c72cd979be4553f2310ed6f0
Alaude30 <kuramamode11@gmail.com>
2018-09-13 20:56:30 +0800

    add some style in chats heads

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { UsersModel } from '../../models/users.model';
 
 @Component({
   selector: 'users-list-component',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.scss']
 })
-export class UsersListComponent {}
+export class UsersListComponent implements OnInit {
+  users: UsersModel[];
+  route = 'users';
+  constructor(private dataService: DataService) {}
+
+  getUsers(): void {
+    this.dataService.getData(this.route).subscribe(
+      response => {
+        this.users = response;
+      },
+      err => console.log('Error occured while fetching users', err)
+    );
+  }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+}
